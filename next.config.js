5059f7a7615832c2e3d6c902626e259ba6a1b835
kirandeep17 <62733570+kirandeep17@users.noreply.github.com>
2023-08-14 16:49:04 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   env: {
-    API_URL: 'http://18.119.158.215:8000', // Your API URL
+    API_URL: 'http://3.145.32.235:8000', // Your API URL
     // Add other constant variables here
   },
 };
